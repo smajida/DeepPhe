3c6c866f83dafb2049c6462095242173849f2ca3
girish_chavan <girishc@gmail.com>
2015-07-31 16:33:49 -0400

    Pipeline runner for Doc summarizer

---
+++ deepphe-uima/src/main/java/org/healthnlp/deepphe/uima/pipelines/DocumentSummarizerPipelineRunner.java
@@ -0,0 +1,61 @@
+package org.healthnlp.deepphe.uima.pipelines;
+
+import java.io.IOException;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.apache.ctakes.cancer.pipelines.CancerPipelineRunner;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import com.lexicalscope.jewel.cli.CliFactory;
+import com.lexicalscope.jewel.cli.Option;
+
+@Immutable
+final public class DocumentSummarizerPipelineRunner {
+   private DocumentSummarizerPipelineRunner() {
+   }
+
+   static interface Options{
+      @Option(
+            shortName = "i",
+            description = "specify the path to the directory containing the clinical notes to be processed" )
+      public String getInputDirectory();
+
+      @Option(
+            shortName = "o",
+            description = "specify the path to the directory where the output xmi files are to be saved" )
+      public String getOutputDirectory();
+   }
+
+   
+   public static void runCancerPipeline( final String inputDirectory,
+                                         final String outputDirectory ) throws UIMAException, IOException {
+      final CollectionReader collectionReader = CancerPipelineRunner.createFilesInDirectoryReader( inputDirectory );
+      final AnalysisEngineDescription analysisEngineDescription = CancerPipelineRunner.getPipelineDescription();
+      final AnalysisEngine xmiWriter = CancerPipelineRunner.createXMIWriter( outputDirectory );
+      runCancerPipeline( collectionReader, analysisEngineDescription, xmiWriter );
+   }
+
+   public static void runCancerPipeline( final CollectionReader collectionReader,
+                                         final AnalysisEngineDescription analysisEngineDescription,
+                                         final AnalysisEngine outputWriter ) throws UIMAException, IOException {
+      SimplePipeline.runPipeline( collectionReader,
+            AnalysisEngineFactory.createEngine( analysisEngineDescription ),
+            outputWriter );
+   }
+
+   public static void main( final String... args ) throws UIMAException, IOException {
+      final Options options = CliFactory.parseArguments( Options.class, args );
+      runCancerPipeline( options.getInputDirectory(), options.getOutputDirectory() );
+   }
+
+
+
+
+
+}
