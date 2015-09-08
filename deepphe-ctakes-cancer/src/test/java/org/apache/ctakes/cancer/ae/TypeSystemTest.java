953a6fe02646294d69476c1463e0f90e0d905964
peistation <chenpei@apache.org>
2015-09-08 14:56:04 -0400

    Add test for type system
    Print out Type System that is currently being used at runtime.

---
+++ deepphe-ctakes-cancer/src/test/java/org/apache/ctakes/cancer/ae/TypeSystemTest.java
@@ -0,0 +1,28 @@
+package org.apache.ctakes.cancer.ae;
+
+import static org.junit.Assert.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class TypeSystemTest {
+
+	@Test
+	public void test() throws Exception {
+		testTypeSystem();
+	}
+	
+	public static void testTypeSystem() throws Exception {
+		TypeSystemDescription typesystem = TypeSystemDescriptionFactory
+				.createTypeSystemDescription();
+		typesystem.toXML(System.out);
+		//typesystem.toXML(new FileWriter("SuperDuperTypeSystem.xml"));
+	}
+
+}
