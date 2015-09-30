7d90d33f03e5bc2ad408ba02073eb293633feec2
tmill <tim.miller@gmail.com>
2015-09-30 06:41:50 -0400

    Added wrapper for CVD so we can call it from within the project.

---
+++ deepphe-uima/src/main/java/org/healthnlp/deepphe/uima/cvd/DeepPheCvd.java
@@ -0,0 +1,11 @@
+package org.healthnlp.deepphe.uima.cvd;
+
+import org.apache.uima.tools.cvd.CVD;
+
+public class DeepPheCvd {
+	
+	public static void main(String[] args) {
+	  CVD.main(args);
+	}
+
+}
