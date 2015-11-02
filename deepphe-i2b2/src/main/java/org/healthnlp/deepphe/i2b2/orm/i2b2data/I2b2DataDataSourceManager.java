d13a14a99caa6fc34b1eefd4411054cd62fb8355
kjm84 <kjm84@pitt.edu>
2015-11-02 13:46:03 -0500

    Made I2B2 database tests conditional on database existance.

--- deepphe-i2b2/src/main/java/org/healthnlp/deepphe/i2b2/orm/i2b2data/I2b2DataDataSourceManager.java
+++ deepphe-i2b2/src/main/java/org/healthnlp/deepphe/i2b2/orm/i2b2data/I2b2DataDataSourceManager.java
@@ -5,236 +5,273 @@
  */
 package org.healthnlp.deepphe.i2b2.orm.i2b2data;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Set;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.reflect.ClassPath;
 import com.google.common.reflect.ClassPath.ClassInfo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class I2b2DataDataSourceManager {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(I2b2DataDataSourceManager.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(I2b2DataDataSourceManager.class);
 
-    private String hibernateDialect = org.hibernate.dialect.Oracle10gDialect.class
-            .getName();
-    private String hibernateDriver = oracle.jdbc.OracleDriver.class.getName();
-    private String hibernateConnectionUrl = "jdbc:oracle:thin:@dbmi-i2b2-dev05.dbmi.pitt.edu:1521:XE";
-    private String hibernateUserName = "i2b2demodata";
-    private String hibernateUserPassword = "demouser";
+	private String hibernateDialect;
+	private String hibernateDriver;
+	private String hibernateConnectionUrl;
+	private String hibernateUserName;
+	private String hibernateUserPassword;
 
-    //Very dangerous to have this set to create or update. Only available for 
-    //explicit use by the installer or other utility methods. Must always 
-    //default to null to avoid accidental database modifications.
-    private String hibernateHbm2ddlAuto = "none";
+	// Very dangerous to have this set to create or update. Only available for
+	// explicit use by the installer or other utility methods. Must always
+	// default to null to avoid accidental database modifications.
+	private String hibernateHbm2ddlAuto = "none";
 
-    private String hibernateShowSql = "true";
-    private String hibernateCacheUseSecondLevelCache = "false";
+	private String hibernateShowSql = "true";
+	private String hibernateCacheUseSecondLevelCache = "false";
 
-    private Configuration configuration;
+	private Configuration configuration;
 
-    private SessionFactory sessionFactory;
+	private SessionFactory sessionFactory;
 
-    private Session session;
+	private Session session;
 
-    public I2b2DataDataSourceManager() {
-    }
+	public I2b2DataDataSourceManager() {
+	}
 
-    private void buildConfiguration() {
-        configuration = new Configuration();
-        configuration.setProperty("hibernate.dialect", getHibernateDialect());
-        configuration.setProperty("hibernate.connection.driver_class",
-                getHibernateDriver());
-        configuration.setProperty("hibernate.show_sql", getHibernateShowSql());
-        configuration.setProperty("hibernate.connection.url",
-                getHibernateConnectionUrl());
-        configuration.setProperty("hibernate.connection.username",
-                getHibernateUserName());
-        configuration.setProperty("hibernate.connection.password",
-                getHibernateUserPassword());
-        configuration.setProperty("hibernate.cache.use_second_level_cache", getHibernateCacheUseSecondLevelCache());
-        if (getHibernateHbm2ddlAuto() != null) {
-            configuration.setProperty("hibernate.hbm2ddl.auto",
-                    getHibernateHbm2ddlAuto());
-        }
-        addAnnotatedClasses();
-    }
+	private void buildConfiguration() {
 
-    /**
-     * get or create configuration object to further customize it before calling
-     * getSession()
-     *
-     * @return
-     */
-    public Configuration getConfiguration() {
-        if (configuration == null) {
-            buildConfiguration();
-        }
-        return configuration;
-    }
+		try {
+			URL dbPropertiesUrl = getClass().getResource("jdbc.properties");
+			Properties dbProperties = new Properties();
+			FileReader reader = new FileReader(dbPropertiesUrl.getPath());
+			dbProperties.load(reader);
 
-    private void addAnnotatedClasses() {
-        configuration.addAnnotatedClass(ObservationFact.class);
-        configuration.addAnnotatedClass(ProviderDimension.class);
-        configuration.addAnnotatedClass(VisitDimension.class);
-        configuration.addAnnotatedClass(PatientDimension.class);
-        configuration.addAnnotatedClass(ConceptDimension.class);
+			setHibernateDriver(dbProperties.getProperty("driver",
+					oracle.jdbc.OracleDriver.class.getName()));
+			setHibernateDialect(dbProperties.getProperty("dialect",
+					org.hibernate.dialect.Oracle10gDialect.class.getName()));
+			setHibernateConnectionUrl(dbProperties.getProperty("url",
+					"jdbc:oracle:thin:@dbmi-i2b2-dev05.dbmi.pitt.edu:1521:XE"));
+			setHibernateUserName(dbProperties.getProperty("user",
+					"i2b2demodata"));
+			setHibernateUserPassword(dbProperties.getProperty("password",
+					"demouser"));
 
-    }
+			configuration = new Configuration();
+			configuration.setProperty("hibernate.dialect",
+					getHibernateDialect());
+			configuration.setProperty("hibernate.connection.driver_class",
+					getHibernateDriver());
+			configuration.setProperty("hibernate.show_sql",
+					getHibernateShowSql());
+			configuration.setProperty("hibernate.connection.url",
+					getHibernateConnectionUrl());
+			configuration.setProperty("hibernate.connection.username",
+					getHibernateUserName());
+			configuration.setProperty("hibernate.connection.password",
+					getHibernateUserPassword());
+			configuration.setProperty("hibernate.cache.use_second_level_cache",
+					getHibernateCacheUseSecondLevelCache());
+			if (getHibernateHbm2ddlAuto() != null) {
+				configuration.setProperty("hibernate.hbm2ddl.auto",
+						getHibernateHbm2ddlAuto());
+			}
+			addAnnotatedClasses();
 
-    public void addAnnotatedClsesForPackage() {
-        logger.debug("Entered addAnnotatedClsesForPackage for " + getClass().getName());
-        try {
-            String pkgName = getClass().getPackage().getName();
-            ClassPath classPath = ClassPath.from(getClass().getClassLoader());
-            final ArrayList<Class<?>> clses = new ArrayList<>();
-            Set<ClassInfo> clsInfos = classPath
-            .getTopLevelClassesRecursive(pkgName);
-            for (ClassInfo clsInfo : clsInfos) {
-            	if (!clsInfo.getName().equals(getClass().getName())) {
-            		clses.add(clsInfo.load());
-            	}
-            }
-            if (clses.isEmpty()) {
-                logger.error("Failed to load hibernate clses");
-            } else {
-            	for (Class<?> cls : clses) {
-            		 System.err.println("Configuring " + cls.getName() + " into Hibernate");
-                     logger.info("Configuring " + cls.getName() + " into Hibernate");
-                     configuration.addAnnotatedClass(cls);
-            	}
-            }
-        } catch (IOException e) {
-        }
-        logger.debug("Exited addAnnotatedClsesForPackage for " + getClass().getName());
-    }
+		} catch (IOException x) {
+			System.err.println("Error connecting to I2B2 database.");
+		}
 
-    /*
-     * SessionFactory
-     */
-    private boolean buildSessionFactory(Configuration configuration) {
-        sessionFactory = configuration.buildSessionFactory();
-        return !sessionFactory.isClosed();
-    }
+	}
 
-    /*
-     * Session
-     */
-    public Session getSession() {
-        if (session == null) {
-            if (configuration == null) {
-                buildConfiguration();
-            }
-            if (sessionFactory == null) {
-                buildSessionFactory(configuration);
-            }
+	/**
+	 * get or create configuration object to further customize it before calling
+	 * getSession()
+	 *
+	 * @return
+	 */
+	public Configuration getConfiguration() {
+		if (configuration == null) {
+			buildConfiguration();
+		}
+		return configuration;
+	}
 
-            session = sessionFactory.openSession();
-        }
-        return session;
-    }
+	private void addAnnotatedClasses() {
+		configuration.addAnnotatedClass(ObservationFact.class);
+		configuration.addAnnotatedClass(ProviderDimension.class);
+		configuration.addAnnotatedClass(VisitDimension.class);
+		configuration.addAnnotatedClass(PatientDimension.class);
+		configuration.addAnnotatedClass(ConceptDimension.class);
 
-    /*
-     * Clean up
-     */
-    private void closeSession() throws HibernateException {
-        if (session != null && session.isOpen()) {
-            session.close();
-            session = null;
-        }
-    }
+	}
 
-    private void closeSessionFactory() throws HibernateException {
-        if (sessionFactory != null && !sessionFactory.isClosed()) {
-            sessionFactory.close();
-        }
-        sessionFactory = null;
-    }
+	public void addAnnotatedClsesForPackage() {
+		logger.debug("Entered addAnnotatedClsesForPackage for "
+				+ getClass().getName());
+		try {
+			String pkgName = getClass().getPackage().getName();
+			ClassPath classPath = ClassPath.from(getClass().getClassLoader());
+			final ArrayList<Class<?>> clses = new ArrayList<>();
+			Set<ClassInfo> clsInfos = classPath
+					.getTopLevelClassesRecursive(pkgName);
+			for (ClassInfo clsInfo : clsInfos) {
+				if (!clsInfo.getName().equals(getClass().getName())) {
+					clses.add(clsInfo.load());
+				}
+			}
+			if (clses.isEmpty()) {
+				logger.error("Failed to load hibernate clses");
+			} else {
+				for (Class<?> cls : clses) {
+					System.err.println("Configuring " + cls.getName()
+							+ " into Hibernate");
+					logger.info("Configuring " + cls.getName()
+							+ " into Hibernate");
+					configuration.addAnnotatedClass(cls);
+				}
+			}
+		} catch (IOException e) {
+		}
+		logger.debug("Exited addAnnotatedClsesForPackage for "
+				+ getClass().getName());
+	}
 
-    public void destroy() {
-        closeSession();
-        closeSessionFactory();
-        logger.debug("destroyed " + getClass().getName() + " : session and sessionFactory removed.");
-    }
+	/*
+	 * SessionFactory
+	 */
+	private boolean buildSessionFactory(Configuration configuration) {
+		sessionFactory = configuration.buildSessionFactory();
+		return !sessionFactory.isClosed();
+	}
 
-    @Override
-    protected void finalize() throws Throwable {
-        destroy();
-        super.finalize();
-    }
+	/*
+	 * Session
+	 */
+	public Session getSession() {
+		try {
+			if (session == null) {
+				if (configuration == null) {
+					buildConfiguration();
+				}
+				if (sessionFactory == null) {
+					buildSessionFactory(configuration);
+				}
 
-    public String getHibernateDialect() {
-        return hibernateDialect;
-    }
+				session = sessionFactory.openSession();
+			}
+		} catch (Exception x) {
+			System.err.println("Failed to get database session.");
+			session = null;
+		}
+		return session;
+	}
 
-    public void setHibernateDialect(String hibernateDialect) {
-        this.hibernateDialect = hibernateDialect;
-    }
+	/*
+	 * Clean up
+	 */
+	private void closeSession() throws HibernateException {
+		if (session != null && session.isOpen()) {
+			session.close();
+			session = null;
+		}
+	}
 
-    public String getHibernateDriver() {
-        return hibernateDriver;
-    }
+	private void closeSessionFactory() throws HibernateException {
+		if (sessionFactory != null && !sessionFactory.isClosed()) {
+			sessionFactory.close();
+		}
+		sessionFactory = null;
+	}
 
-    public void setHibernateDriver(String hibernateDriver) {
-        this.hibernateDriver = hibernateDriver;
-    }
+	public void destroy() {
+		closeSession();
+		closeSessionFactory();
+		logger.debug("destroyed " + getClass().getName()
+				+ " : session and sessionFactory removed.");
+	}
 
-    public String getHibernateConnectionUrl() {
-        return hibernateConnectionUrl;
-    }
+	@Override
+	protected void finalize() throws Throwable {
+		destroy();
+		super.finalize();
+	}
 
-    public void setHibernateConnectionUrl(String hibernateConnectionUrl) {
-        this.hibernateConnectionUrl = hibernateConnectionUrl;
-    }
+	public String getHibernateDialect() {
+		return hibernateDialect;
+	}
 
-    public String getHibernateUserName() {
-        return hibernateUserName;
-    }
+	public void setHibernateDialect(String paramHibernateDialect) {
+		this.hibernateDialect = paramHibernateDialect;
+	}
 
-    public void setHibernateUserName(String hibernateUserName) {
-        this.hibernateUserName = hibernateUserName;
-    }
+	public String getHibernateDriver() {
+		return hibernateDriver;
+	}
 
-    public String getHibernateUserPassword() {
-        return hibernateUserPassword;
-    }
+	public void setHibernateDriver(String hibernateDriver) {
+		this.hibernateDriver = hibernateDriver;
+	}
 
-    public void setHibernateUserPassword(String hibernateUserPassword) {
-        this.hibernateUserPassword = hibernateUserPassword;
-    }
+	public String getHibernateConnectionUrl() {
+		return hibernateConnectionUrl;
+	}
 
-    public String getHibernateHbm2ddlAuto() {
-        return hibernateHbm2ddlAuto;
-    }
+	public void setHibernateConnectionUrl(String hibernateConnectionUrl) {
+		this.hibernateConnectionUrl = hibernateConnectionUrl;
+	}
 
-    public void setHibernateHbm2ddlAuto(String hibernateHbm2ddlAuto) {
-        this.hibernateHbm2ddlAuto = hibernateHbm2ddlAuto;
-    }
+	public String getHibernateUserName() {
+		return hibernateUserName;
+	}
 
-    public String getHibernateShowSql() {
-        return hibernateShowSql;
-    }
+	public void setHibernateUserName(String hibernateUserName) {
+		this.hibernateUserName = hibernateUserName;
+	}
 
-    public void setHibernateShowSql(String hibernateShowSql) {
-        this.hibernateShowSql = hibernateShowSql;
-    }
+	public String getHibernateUserPassword() {
+		return hibernateUserPassword;
+	}
 
-    public String getHibernateCacheUseSecondLevelCache() {
-        return hibernateCacheUseSecondLevelCache;
-    }
+	public void setHibernateUserPassword(String hibernateUserPassword) {
+		this.hibernateUserPassword = hibernateUserPassword;
+	}
 
-    public void setHibernateCacheUseSecondLevelCache(
-            String hibernateCacheUseSecondLevelCache) {
-        this.hibernateCacheUseSecondLevelCache = hibernateCacheUseSecondLevelCache;
-    }
+	public String getHibernateHbm2ddlAuto() {
+		return hibernateHbm2ddlAuto;
+	}
+
+	public void setHibernateHbm2ddlAuto(String hibernateHbm2ddlAuto) {
+		this.hibernateHbm2ddlAuto = hibernateHbm2ddlAuto;
+	}
+
+	public String getHibernateShowSql() {
+		return hibernateShowSql;
+	}
+
+	public void setHibernateShowSql(String hibernateShowSql) {
+		this.hibernateShowSql = hibernateShowSql;
+	}
+
+	public String getHibernateCacheUseSecondLevelCache() {
+		return hibernateCacheUseSecondLevelCache;
+	}
+
+	public void setHibernateCacheUseSecondLevelCache(
+			String hibernateCacheUseSecondLevelCache) {
+		this.hibernateCacheUseSecondLevelCache = hibernateCacheUseSecondLevelCache;
+	}
 
 }
