d8c25603fb38dced4a8dc495903233c1b644ca22
kjm84 <kjm84@pitt.edu>
2016-01-25 10:11:42 -0500

    Added test cases for Tnm

--- deepphe-uima/src/test/java/org/healthnlp/deepphe/summarization/drools/kb/impl/TesterBrcaTnm.java
+++ deepphe-uima/src/test/java/org/healthnlp/deepphe/summarization/drools/kb/impl/TesterBrcaTnm.java
@@ -19,6 +19,9 @@ import org.drools.runtime.rule.FactHandle;
 import org.healthnlp.deepphe.summarization.drools.kb.KbGoal;
 import org.healthnlp.deepphe.summarization.drools.kb.KbPatient;
 import org.healthnlp.deepphe.summarization.drools.kb.M1;
+import org.healthnlp.deepphe.summarization.drools.kb.Pn1;
+import org.healthnlp.deepphe.summarization.drools.kb.Pn1mi;
+import org.healthnlp.deepphe.summarization.drools.kb.Tis;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -64,12 +67,12 @@ public class TesterBrcaTnm {
 	}
 
 	@Test
-	public void testMoneDashOne() {
+	public void testTisDashOne() {
 
 		int idCounter = 0;
 
 		System.out.println("\nRunning test M1-1\n");
-		
+
 		final List<FactHandle> factHandles = new ArrayList<FactHandle>();
 
 		KbGoal goal = new KbGoal();
@@ -77,16 +80,148 @@ public class TesterBrcaTnm {
 		goal.setName("extract-tnm");
 		goal.setIsActive(1);
 		factHandles.add(session.insert(goal));
-	
+
 		KbPatient patient = new KbPatient();
 		patient.setId(idCounter++);
 		factHandles.add(session.insert(patient));
-		
+
+		Tis tis = new TisImpl();
+		tis.setId(idCounter++);
+		tis.setSummarizableId(patient.getId());
+		factHandles.add(session.insert(tis));
+
+		long numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, factHandles.size());
+
+		final String[] rulesToTest = { "003_Tis-1" };
+		CustomAgendaFilter customAgendaFilter = new CustomAgendaFilter(
+				rulesToTest);
+		int numRulesFired = session.fireAllRules(customAgendaFilter);
+		assertEquals(1, numRulesFired);
+
+		numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, factHandles.size() + 7);
+
+		for (FactHandle factHandle : factHandles) {
+			session.retract(factHandle);
+		}
+
+		numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, 7);
+	}
+
+	@Test
+	public void testPnOneMiDashOne() {
+
+		int idCounter = 0;
+
+		System.out.println("\nRunning test pN1m1-1\n");
+
+		final List<FactHandle> factHandles = new ArrayList<FactHandle>();
+
+		KbGoal goal = new KbGoal();
+		goal.setId(idCounter++);
+		goal.setName("extract-tnm");
+		goal.setIsActive(1);
+		factHandles.add(session.insert(goal));
+
+		KbPatient patient = new KbPatient();
+		patient.setId(idCounter++);
+		factHandles.add(session.insert(patient));
+
+		Pn1mi tis = new Pn1miImpl();
+		tis.setId(idCounter++);
+		tis.setSummarizableId(patient.getId());
+		factHandles.add(session.insert(tis));
+
+		long numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, factHandles.size());
+
+		final String[] rulesToTest = { "003_pN1m1-1" };
+		CustomAgendaFilter customAgendaFilter = new CustomAgendaFilter(
+				rulesToTest);
+		int numRulesFired = session.fireAllRules(customAgendaFilter);
+		assertEquals(1, numRulesFired);
+
+		numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, factHandles.size() + 9);
+
+		for (FactHandle factHandle : factHandles) {
+			session.retract(factHandle);
+		}
+
+		numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, 9);
+	}
+
+	@Test
+	public void testPnOneDashOne() {
+
+		int idCounter = 0;
+
+		System.out.println("\nRunning test 003_pN1-1\n");
+
+		final List<FactHandle> factHandles = new ArrayList<FactHandle>();
+
+		KbGoal goal = new KbGoal();
+		goal.setId(idCounter++);
+		goal.setName("extract-tnm");
+		goal.setIsActive(1);
+		factHandles.add(session.insert(goal));
+
+		KbPatient patient = new KbPatient();
+		patient.setId(idCounter++);
+		factHandles.add(session.insert(patient));
+
+		Pn1 tis = new Pn1Impl();
+		tis.setId(idCounter++);
+		tis.setSummarizableId(patient.getId());
+		factHandles.add(session.insert(tis));
+
+		long numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, factHandles.size());
+
+		final String[] rulesToTest = { "003_pN1-1" };
+		CustomAgendaFilter customAgendaFilter = new CustomAgendaFilter(
+				rulesToTest);
+		int numRulesFired = session.fireAllRules(customAgendaFilter);
+		assertEquals(1, numRulesFired);
+
+		numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, factHandles.size() + 9);
+
+		for (FactHandle factHandle : factHandles) {
+			session.retract(factHandle);
+		}
+
+		numberOfFactsInSession = session.getFactCount();
+		assertEquals(numberOfFactsInSession, 9);
+	}
+
+	@Test
+	public void testMoneDashOne() {
+
+		int idCounter = 0;
+
+		System.out.println("\nRunning test M1-1\n");
+
+		final List<FactHandle> factHandles = new ArrayList<FactHandle>();
+
+		KbGoal goal = new KbGoal();
+		goal.setId(idCounter++);
+		goal.setName("extract-tnm");
+		goal.setIsActive(1);
+		factHandles.add(session.insert(goal));
+
+		KbPatient patient = new KbPatient();
+		patient.setId(idCounter++);
+		factHandles.add(session.insert(patient));
+
 		M1 m1 = new M1Impl();
 		m1.setId(idCounter++);
 		m1.setSummarizableId(patient.getId());
 		factHandles.add(session.insert(m1));
-	
+
 		long numberOfFactsInSession = session.getFactCount();
 		assertEquals(numberOfFactsInSession, factHandles.size());
 
@@ -95,17 +230,16 @@ public class TesterBrcaTnm {
 				rulesToTest);
 		int numRulesFired = session.fireAllRules(customAgendaFilter);
 		assertEquals(1, numRulesFired);
-	
+
 		numberOfFactsInSession = session.getFactCount();
 		assertEquals(numberOfFactsInSession, factHandles.size() + 3);
 
 		for (FactHandle factHandle : factHandles) {
 			session.retract(factHandle);
 		}
-		
+
 		numberOfFactsInSession = session.getFactCount();
 		assertEquals(numberOfFactsInSession, 3);
 	}
-
 
 }
