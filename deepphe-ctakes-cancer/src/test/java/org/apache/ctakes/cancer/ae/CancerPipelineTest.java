75f43a5f5755d54de29de2fe3739e113c74db0ea
seanfinan <sean.finan@tch.harvard.edu>
2016-02-11 11:44:08 -0500

    These are not proper unit tests.  They clutter the build/test output so I am just commenting out everything.

--- deepphe-ctakes-cancer/src/test/java/org/apache/ctakes/cancer/ae/CancerPipelineTest.java
+++ deepphe-ctakes-cancer/src/test/java/org/apache/ctakes/cancer/ae/CancerPipelineTest.java
@@ -20,40 +20,40 @@ public class CancerPipelineTest {
    static private final String DATA_DIRECTORY_PATH = "data/sample/docs";
 
 
-   @Test
-   public void test() throws ResourceInitializationException, AnalysisEngineProcessException {
-      File dataDirectory;
-      try {
-         dataDirectory = FileLocator.locateFile( DATA_DIRECTORY_PATH );
-      } catch ( IOException ioE ) {
-         throw new ResourceInitializationException( ioE );
-      }
-      final File[] files = dataDirectory.listFiles();
-      if ( files == null || files.length == 0 ) {
-         LOGGER.error( "No Files in directory: " + dataDirectory.getPath() );
-         System.exit( 1 );
-      }
-      // Get the default pipeline
-      final AnalysisEngine analysisEngine = CancerPipelineFactory.createPipelineEngine();
-
-      final StringBuilder sb = new StringBuilder();
-      final JCas jcas = analysisEngine.newJCas();
-      for ( File file : files ) {
-         LOGGER.info( "Processing: " + file.getName() );
-         try ( BufferedReader br = new BufferedReader( new FileReader( file ) ) ) {
-            String line = br.readLine();
-            while ( line != null ) {
-               sb.append( line ).append( '\n' );
-               line = br.readLine();
-            }
-         } catch ( IOException ioE ) {
-            throw new AnalysisEngineProcessException( ioE );
-         }
-         jcas.setDocumentText( sb.toString() );
-         sb.setLength( 0 );
-         analysisEngine.process( jcas );
-         jcas.reset();
-      }
-   }
+//   @Test
+//   public void test() throws ResourceInitializationException, AnalysisEngineProcessException {
+//      File dataDirectory;
+//      try {
+//         dataDirectory = FileLocator.locateFile( DATA_DIRECTORY_PATH );
+//      } catch ( IOException ioE ) {
+//         throw new ResourceInitializationException( ioE );
+//      }
+//      final File[] files = dataDirectory.listFiles();
+//      if ( files == null || files.length == 0 ) {
+//         LOGGER.error( "No Files in directory: " + dataDirectory.getPath() );
+//         System.exit( 1 );
+//      }
+//      // Get the default pipeline
+//      final AnalysisEngine analysisEngine = CancerPipelineFactory.createPipelineEngine();
+//
+//      final StringBuilder sb = new StringBuilder();
+//      final JCas jcas = analysisEngine.newJCas();
+//      for ( File file : files ) {
+//         LOGGER.info( "Processing: " + file.getName() );
+//         try ( BufferedReader br = new BufferedReader( new FileReader( file ) ) ) {
+//            String line = br.readLine();
+//            while ( line != null ) {
+//               sb.append( line ).append( '\n' );
+//               line = br.readLine();
+//            }
+//         } catch ( IOException ioE ) {
+//            throw new AnalysisEngineProcessException( ioE );
+//         }
+//         jcas.setDocumentText( sb.toString() );
+//         sb.setLength( 0 );
+//         analysisEngine.process( jcas );
+//         jcas.reset();
+//      }
+//   }
 
 }
