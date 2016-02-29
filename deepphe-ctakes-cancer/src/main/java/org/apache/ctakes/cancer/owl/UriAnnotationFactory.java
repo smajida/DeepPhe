906ef16ddb1964e594f896fb8227cc2da7725e2f
seanfinan <sean.finan@tch.harvard.edu>
2016-02-29 11:37:37 -0500

    More javadocs

--- deepphe-ctakes-cancer/src/main/java/org/apache/ctakes/cancer/owl/UriAnnotationFactory.java
+++ deepphe-ctakes-cancer/src/main/java/org/apache/ctakes/cancer/owl/UriAnnotationFactory.java
@@ -16,6 +16,11 @@ import javax.annotation.concurrent.Immutable;
 import static org.apache.ctakes.typesystem.type.constants.CONST.*;
 
 /**
+ * Factory class that should be used to create annotations from uris.
+ * Created annotations will have a class type based upon the semantic group of the iclass with the given uri.
+ * The method used to create an annotation from a uri is {@link #createIdentifiedAnnotation(JCas, int, int, String)}.
+ * There is also a method that can be used to create only a UmlsConcept with a uri,
+ * which can be used by external code to create a full annotation.
  * @author SPF , chip-nlp
  * @version %I%
  * @since 2/25/2016
@@ -28,7 +33,13 @@ final public class UriAnnotationFactory {
    private UriAnnotationFactory() {
    }
 
-
+   /**
+    * @param jcas        ye olde ...
+    * @param beginOffset the offset of the first character of the annotation within text
+    * @param endOffset   the offset of the last character (+1) of the annotation within text
+    * @param uri         definitive uri for the annotation to be created
+    * @return appropriate Semantic group subclass of IdentifiedAnnotation with a populated ontology concept array
+    */
    static public IdentifiedAnnotation createIdentifiedAnnotation( final JCas jcas, final int beginOffset,
                                                                   final int endOffset, final String uri ) {
       final IClass iClass = OwlOntologyConceptUtil.getIClass( uri );
@@ -41,6 +52,7 @@ final public class UriAnnotationFactory {
       annotation.setTypeID( semanticGroupId );
       annotation.setBegin( beginOffset );
       annotation.setEnd( endOffset );
+      //  As there are currently only 2 discovery techniques this is not exact, but "gold" may be better than "lookup"
       annotation.setDiscoveryTechnique( NE_DISCOVERY_TECH_GOLD_ANNOTATION );
       final UmlsConcept umlsConcept = createUmlsConcept( jcas, iClass, uri );
       final FSArray conceptArray = new FSArray( jcas, 1 );
@@ -50,6 +62,12 @@ final public class UriAnnotationFactory {
       return annotation;
    }
 
+   /**
+    * @param jcas ye olde ...
+    * @param cTakesSemantic integer code for the ctakes semantic group to which the annotation belongs
+    * @return instance of some IdentifiedAnnotation subclass for the given cTakesSemantic
+    * or a generic EventMention if none can be derived
+    */
    static private IdentifiedAnnotation createSemanticAnnotation( final JCas jcas, final int cTakesSemantic ) {
       switch ( cTakesSemantic ) {
          case NE_TYPE_ID_DRUG: {
@@ -74,6 +92,12 @@ final public class UriAnnotationFactory {
       return new EntityMention( jcas );
    }
 
+   /**
+    *
+    * @param jcas ye olde ...
+    * @param uri uri used to populate the UmlsConcept information
+    * @return UmlsConcept populated with cui, tui, uri, preferred text
+    */
    static public UmlsConcept createUmlsConcept( final JCas jcas, final String uri ) {
       final IClass iClass = OwlOntologyConceptUtil.getIClass( uri );
       if ( iClass == null ) {
@@ -85,7 +109,13 @@ final public class UriAnnotationFactory {
       return createUmlsConcept( jcas, iClass, uri );
    }
 
-
+   /**
+    *
+    * @param jcas ye olde ...
+    * @param iClass for the umls concept.  Usually from the uri.  Cui, Tui, Preferred text are from the iclass
+    * @param uri uri used to populate the UmlsConcept information
+    * @return UmlsConcept populated with cui, tui, uri, preferred text
+    */
    static private UmlsConcept createUmlsConcept( final JCas jcas, final IClass iClass, final String uri ) {
       final String cui = OwlParserUtil.getCui( iClass );
       final String tui = OwlParserUtil.getTui( iClass );
