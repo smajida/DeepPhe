33f5f52a436825195eafe7ce4b290197cfe7b553
girish_chavan <girishc@gmail.com>
2016-04-08 10:40:22 -0400

    Ignoring broken test for now

--- deepphe-ctakes-cancer/src/test/java/org/apache/ctakes/cancer/ae/TypeSystemTest.java
+++ deepphe-ctakes-cancer/src/test/java/org/apache/ctakes/cancer/ae/TypeSystemTest.java
@@ -8,21 +8,19 @@ import java.io.IOException;
 import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
 public class TypeSystemTest {
 
-//	@Test
-//	public void test() throws Exception {
-//		testTypeSystem();
-//	}
-//
-//	public static void testTypeSystem() throws Exception {
-//		TypeSystemDescription typesystem = TypeSystemDescriptionFactory
-//				.createTypeSystemDescription();
-//		typesystem.toXML(System.out);
-//		//typesystem.toXML(new FileWriter("SuperDuperTypeSystem.xml"));
-//	}
+    @Ignore
+    @Test
+    public static void testTypeSystem() throws Exception {
+		TypeSystemDescription typesystem = TypeSystemDescriptionFactory
+				.createTypeSystemDescription();
+		typesystem.toXML(System.out);
+		//typesystem.toXML(new FileWriter("SuperDuperTypeSystem.xml"));
+	}
 
 }
