53c0e7abb9d039ac2184f2eef0ad989ee0a21354
seanfinan <sean.finan@tch.harvard.edu>
2016-06-10 14:33:29 -0400

    Increased test finder window Changed test by string to test by pattern added exclusion of  hidden files in filetreereader

--- deepphe-ctakes-cancer/src/main/java/org/apache/ctakes/cancer/ae/GenericByTestFinder.java
+++ deepphe-ctakes-cancer/src/main/java/org/apache/ctakes/cancer/ae/GenericByTestFinder.java
@@ -1,16 +1,16 @@
 package org.apache.ctakes.cancer.ae;
 
 
-import org.apache.ctakes.cancer.relation.NeoplasmRelationFactory;
-import org.apache.ctakes.core.ontology.OwlOntologyConceptUtil;
+import org.apache.ctakes.typesystem.type.textsem.DiseaseDisorderMention;
 import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
 import org.apache.log4j.Logger;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.regex.Pattern;
 
 /**
  * @author SPF , chip-nlp
@@ -21,8 +21,8 @@ public class GenericByTestFinder extends JCasAnnotator_ImplBase {
 
    static private final Logger LOGGER = Logger.getLogger( "GenericByTestFinder" );
 
-   static private final Collection<String> TEST_FOR_TEXTS
-         = Arrays.asList( "evaluation for", "evaluation of", "tested for", "test for", "test of", "may indicate" );
+   static private final Pattern TEST_FOR_PATTERN
+         = Pattern.compile( "(((evaluat(ion\\s+of)?)|test)(ed\\s+for)?)|(may indicate)" );
 
    /**
     * Removes Metastasis to breast locations
@@ -31,16 +31,12 @@ public class GenericByTestFinder extends JCasAnnotator_ImplBase {
    @Override
    public void process( final JCas jcas ) throws AnalysisEngineProcessException {
       LOGGER.info( "Starting Processing" );
-      final Collection<String> neoplasmUris = NeoplasmRelationFactory.getNeoplasmUris();
-      for ( String neoplasmUri : neoplasmUris ) {
-         final Collection<IdentifiedAnnotation> neoplasms = OwlOntologyConceptUtil
-               .getAnnotationsByUri( jcas, neoplasmUri );
-         for ( IdentifiedAnnotation neoplasm : neoplasms ) {
-            final int begin = Math.max( 0, neoplasm.getBegin() - 15 );
-            final String preceding = jcas.getDocumentText().substring( begin, neoplasm.getBegin() );
-            if ( TEST_FOR_TEXTS.stream().anyMatch( preceding::contains ) ) {
-               neoplasm.setGeneric( true );
-            }
+      final Collection<DiseaseDisorderMention> diseases = JCasUtil.select( jcas, DiseaseDisorderMention.class );
+      for ( IdentifiedAnnotation disease : diseases ) {
+         final int begin = Math.max( 0, disease.getBegin() - 25 );
+         final String preceding = jcas.getDocumentText().substring( begin, disease.getBegin() );
+         if ( TEST_FOR_PATTERN.matcher( preceding ).find() ) {
+            disease.setGeneric( true );
          }
       }
       LOGGER.info( "Finished Processing" );
